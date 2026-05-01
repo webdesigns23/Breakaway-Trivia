0df2eb66da211ac8f4f7d79c7abd87022deae365
Sharmaine Perea <sharmaine.perea23@gmail.com>
2026-04-30 18:21:30 -0600

    added mvp data for teams

---
+++ client/src/data/teams.js
@@ -0,0 +1,69 @@
+const teams = [
+	{
+		id: "1", 
+		name: "Colorado Avalanche",
+		primary_color: "#6F263D",
+		secondary_color: "#236192",
+		accent_color: "#b3b7bb",
+		logo: "https://cdn.freebiesupply.com/logos/large/2x/colorado-avalanche-logo.png"
+	},
+	{
+		id: "2", 
+		name: "Minnesota Wild",
+		primary_color: "#154734",
+		secondary_color: "#A6192E",
+		accent_color: "#EAAA00",
+		logo: "https://cdn.freebiesupply.com/logos/large/2x/minnesota-wild-logo.png"
+	},
+	{
+		id: "3", 
+		name: "Dallas Stars",
+		primary_color: "#006847",
+		secondary_color: "#A4A9AD",
+		accent_color: "#000000",
+		logo: "https://cdn.freebiesupply.com/logos/large/2x/dallas-stars-logo.png"
+	},
+	{
+		id: "4", 
+		name: "Utah Mammoth",
+		primary_color: "#000000",
+		secondary_color: "#FFFFFF",
+		accent_color: "#6CACE4",
+		logo: "https://static.wikia.nocookie.net/logopedia/images/9/9c/UtahMammoth.svg/revision/latest/scale-to-width-down/1000?cb=20250803172824"
+	},
+	{
+		id: "5", 
+		name: "Vegas Golden Knights",
+		primary_color: "#B4975A",
+		secondary_color: "#333F42",
+		accent_color: "#C8102E",
+		logo: "https://cdn.freebiesupply.com/logos/large/2x/vegas-golden-knights-logo.png"
+	},
+	{
+		id: "6", 
+		name: "Carolina Hurricanes",
+		primary_color: "#C8102E",
+		secondary_color: "#000000",
+		accent_color: "#FFFFFF",
+		logo: "https://cdn.freebiesupply.com/logos/large/2x/carolina-hurricanes-logo.png"
+	},
+	{
+		id: "7", 
+		name: "San Jose Sharks",
+		primary_color: "#006D75",
+		secondary_color: "#EA7200",
+		accent_color: "#000000",
+		logo: "https://cdn.freebiesupply.com/logos/large/2x/san-jose-sharks-logo.png"
+	},
+	{
+		id: "8", 
+		name: "Philadelphia Flyers",
+		primary_color: "#F74902",
+		secondary_color: "#000000",
+		accent_color: "#FFFFFF",
+		logo: "https://cdn.freebiesupply.com/logos/large/2x/philadelphia-flyers-logo.png"
+	},
+
+];
+
+export default teams;
